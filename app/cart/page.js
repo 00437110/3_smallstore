47bc3e6cd37ad09d6f4a59b9d18723b1f182b23b
Erick Domínguez <50849788+00437110@users.noreply.github.com>
2025-07-16 20:35:56 -0600

    Chapter 3.2 minute 36, finishing the layout error page, not found page added cart and faq and contact added

---
+++ app/cart/page.js
@@ -0,0 +1,7 @@
+export default function CartPage() {
+  return (
+    <div>
+      2t12131
+    </div>
+  );
+}
---
+++ components/EmailInput.jsx
@@ -0,0 +1,11 @@
+'use client'
+
+export default function EmailInput() {
+    return (
+        <div className="sign-up">
+            <input placeholder="Email Address.." />
+            <button className="button-card">Sign up</button>
+        </div>
+
+    )
+}
