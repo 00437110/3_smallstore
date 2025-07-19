16410113e7d06e8756a3ab4bac6a03d224920541
Erick Domínguez <50849788+00437110@users.noreply.github.com>
2025-07-19 02:16:44 -0600

    Finished Chapter 3.4, changed names of some text for the images

--- components/ImageBanner.jsx
+++ components/ImageBanner.jsx
@@ -1,5 +1,39 @@
+'use client'
+
+import { useState, useRef, useEffect } from "react"
+
 export default function ImageBanner() {
+
+    const [isLoaded, setIsLoaded] = useState(false)
+    const imgRef = useRef()
+
+    useEffect(() => {
+        if (imgRef.current.complete) {
+            setIsLoaded(true)
+        }
+    }, [])
+
     return (
-        <div>   </div>
+        <div className="banner-images">
+            <img className="low-res-img" src="low_res/banner.jpeg"
+                alt="banner-low-res" />
+            <img ref={imgRef} className="high-res-img" src="med_res/banner.png"
+                alt="banner-high-res" style={{ opacity: isLoaded ? 1 : 0 }} onLoad={() => {
+                    //this calll back will be executed when the image is loaded, it will make this invisible, visible
+                    setIsLoaded(true)
+                }} />
+            <div className="cta-btns-container">
+                <div>
+                    <div>
+                        <h3>Welcome to</h3>
+                        <h1>The Smallstore</h1>
+                    </div>
+                    <div>
+                        <button>Shop Stickers</button>
+                        <button>Shop planner</button>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
